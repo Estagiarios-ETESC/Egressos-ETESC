4fc4ec367f1ba481862759f3dff6f6bae3a0debb
Gyaaaaaaan <comteatedes@gmail.com>
2022-07-09 22:27:18 -0300

    Mudanças pequenas na UI

--- script/egresso.js
+++ script/egresso.js
@@ -1,47 +1,68 @@
-const cursosTabs = document.querySelectorAll('.cursos-tabs');
-const cursosContent = document.querySelectorAll('.cursos-content');
-
+const cursosTabs = document.querySelectorAll(".cursos-tabs");
+const cursosContent = document.querySelectorAll(".cursos-content");
 
 function selectItem(e) {
-    removeShow()
-    removeBorder()
-    removeArrow()
-    
-    console.log(this.id)
-    
-    // Adicionando borda para telas pequenas
-    this.classList.add('cursos-container-border')
+  removeShow();
+  removeBorder();
+  removeArrow();
 
+  console.log(this.id);
 
-    // Mostrar a seta
-    const arrowTab = document.querySelector(`#${this.id}-arrow`)
+  // Adicionando borda para telas pequenas
+  this.classList.add("cursos-container-border");
 
-    arrowTab.classList.add('show')
-    
-    // Mostrar conteudo (Egressos)
-    const cursosContentItem = document.querySelector(`#${this.id}-content`)
+  // Mostrar a seta
+  const arrowTab = document.querySelector(`#${this.id}-arrow`);
 
-    cursosContentItem.classList.add('show')
+  arrowTab.classList.add("show");
 
+  // Mostrar conteudo (Egressos)
+  const cursosContentItem = document.querySelector(`#${this.id}-content`);
+
+  cursosContentItem.classList.add("show");
 }
 
 function removeShow() {
-    cursosContent.forEach((item) => item.classList.remove('show'))
+  cursosContent.forEach((item) => item.classList.remove("show"));
 }
 function removeBorder() {
-    cursosTabs.forEach((item) => item.classList.remove('cursos-container-border'))
+  cursosTabs.forEach((item) =>
+    item.classList.remove("cursos-container-border")
+  );
 }
 function removeArrow() {
-    const arrowTabsAll = document.querySelectorAll(`.arrow-div`)
-    arrowTabsAll.forEach((item) => item.classList.remove('show'))
+  const arrowTabsAll = document.querySelectorAll(`.arrow-div`);
+  arrowTabsAll.forEach((item) => item.classList.remove("show"));
 }
 
 // Fazendo os divs ficarem facaveis as apertar tab
-const btnCursos = document.querySelectorAll('.btn-cursos')
-cursosTabs.forEach((item) => item.setAttribute('tabindex', '0'))
-btnCursos.forEach((item) => item.setAttribute('tabindex', '-1'))
-
+const btnCursos = document.querySelectorAll(".btn-cursos");
+cursosTabs.forEach((item) => item.setAttribute("tabindex", "0"));
+btnCursos.forEach((item) => item.setAttribute("tabindex", "-1"));
 
 // Evento de click e focus nos dives de navegação de cursos
-cursosTabs.forEach((item) => item.addEventListener('click', selectItem))
-cursosTabs.forEach((item) => item.addEventListener('focus', selectItem))
+cursosTabs.forEach((item) => item.addEventListener("click", selectItem));
+cursosTabs.forEach((item) => item.addEventListener("focus", selectItem));
+
+// POPUP Handling
+
+const readMoreBtnAll = document.querySelectorAll(".btn-readmore");
+const closePopupBtn = document.querySelector("#popup-btn");
+
+const popupScreen = document.querySelector(".readmore-popup");
+const popupText = document.querySelector(".popup-text");
+
+readMoreBtnAll.forEach((readMoreBtn) => {
+  readMoreBtn.addEventListener("click", (e) => {
+    const current = e.target;
+    document.querySelector("body").classList.toggle("noscroll");
+
+    popupText.innerHTML = current.parentNode.innerHTML;
+    popupScreen.classList.toggle("popup-active");
+  });
+});
+
+closePopupBtn.addEventListener("click", (e) => {
+  document.querySelector("body").classList.toggle("noscroll");
+  popupScreen.classList.toggle("popup-active");
+});
